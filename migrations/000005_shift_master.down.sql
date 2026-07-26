b0507de92301acc451460546b4f627aae962e30d
rajeshbond <rajesh.sntsolutions@gmail.com>
2026-07-26 08:09:08 +0530

    new file:   backup/esp32+backup1.txt 	new file:   migrations/000005_shift_master.down.sql 	new file:   migrations/000005_shift_master.up.sql 	new file:   migrations/000006_shift_schedule.down.sql 	new file:   migrations/000006_shift_schedule.up.sql

---
+++ migrations/000005_shift_master.down.sql
@@ -0,0 +1,7 @@
+DROP INDEX IF EXISTS idx_tenant_shift_master_deleted;
+
+DROP INDEX IF EXISTS idx_tenant_shift_master_active;
+
+DROP INDEX IF EXISTS idx_tenant_shift_master_tenant;
+
+DROP TABLE IF EXISTS tenant_shift_master;
